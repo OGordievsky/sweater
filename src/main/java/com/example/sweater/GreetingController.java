32199c32d7538027d289b02468ec881b388344ca
OGordievsky <og87@bk.ru>
2022-05-20 13:29:06 +0300

    feat: make message controller
    1. connect postgresql
    2. configure main paige

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -1,23 +1,55 @@
 package com.example.sweater;
 
+import com.example.sweater.domain.Message;
+import com.example.sweater.repository.MassageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
 
+    @Autowired
+    private MassageRepo massageRepo;
+
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String,Object> model) {
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Map<String, Object> model) {
         model.put("name", name);
         return "greeting";
     }
 
     @GetMapping
-    public String main(Map<String, Object> model){
-        model.put("some", "hello, LetsCode");
+    public String main(Map<String, Object> model) {
+        Iterable<Message> messages = massageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        massageRepo.save(message);
+        Iterable<Message> messages = massageRepo.findAll();
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = massageRepo.findByTag(filter);
+        } else {
+            messages = massageRepo.findAll();
+        }
+        model.put("messages", messages);
         return "main";
     }
 
---
+++ src/main/java/com/example/sweater/repository/MassageRepo.java
@@ -0,0 +1,10 @@
+package com.example.sweater.repository;
+
+import com.example.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MassageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
