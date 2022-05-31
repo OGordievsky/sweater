8c93415b13a75156210f791f8781ca22994ae47e
OGordievsky <og87@bk.ru>
2022-05-31 13:38:42 +0300

    refactor: UI add Bootstrap
    1. connect Bootstrap css from
    https://getbootstrap.com/docs/4.6/getting-started/introduction/

--- src/main/java/com/example/sweater/domain/User.java
+++ src/main/java/com/example/sweater/domain/User.java
@@ -23,6 +23,10 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
+    public boolean isAdmin() {
+        return roles.contains(Role.ADMIN);
+    }
+
     public Long getId() {
         return id;
     }
