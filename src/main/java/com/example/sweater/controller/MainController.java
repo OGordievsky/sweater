5783e5d5801ada159538221a3af2d753bbfcac86
OGordievsky <og87@bk.ru>
2022-06-14 14:17:12 +0300

    feat: add pagination
    1. refactor MainController
    2. create pager macros

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -5,6 +5,10 @@ import com.example.sweater.domain.User;
 import com.example.sweater.repository.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -39,14 +43,18 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
-        Iterable<Message> messages;
+    public String main(
+            @RequestParam(required = false, defaultValue = "") String filter,
+            Model model,
+            @PageableDefault(sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<Message> page;
         if (filter != null && !filter.isEmpty()) {
-            messages = messageRepo.findByTag(filter);
+            page = messageRepo.findByTag(filter, pageable);
         } else {
-            messages = messageRepo.findAll();
+            page = messageRepo.findAll(pageable);
         }
-        model.addAttribute("messages", messages);
+        model.addAttribute("page", page);
+        model.addAttribute("url", "/main");
         model.addAttribute("filter", filter);
         return "main";
     }
@@ -56,6 +64,7 @@ public class MainController {
                       @Valid Message message,
                       BindingResult bindingResult,
                       Model model,
+                      @PageableDefault(sort = { "id" }, direction = Sort.Direction.DESC) Pageable pageable,
                       @RequestParam("file") MultipartFile file) throws IOException {
         message.setAuthor(user);
 
@@ -68,8 +77,9 @@ public class MainController {
             model.addAttribute("message", null);
             messageRepo.save(message);
         }
-        Iterable<Message> messages = messageRepo.findAll();
-        model.addAttribute("messages", messages);
+        Page<Message> page = messageRepo.findAll(pageable);
+        model.addAttribute("page", page);
+        model.addAttribute("url", "/main");
         model.addAttribute("filter", "");
         return "main";
     }
--- src/main/java/com/example/sweater/repository/MessageRepo.java
+++ src/main/java/com/example/sweater/repository/MessageRepo.java
@@ -1,10 +1,12 @@
 package com.example.sweater.repository;
 
 import com.example.sweater.domain.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface MessageRepo extends CrudRepository<Message, Long> {
-    List<Message> findByTag(String tag);
+
+    Page<Message> findAll(Pageable pageable);
+    Page<Message> findByTag(String tag, Pageable pageable);
 }
